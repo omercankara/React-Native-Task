dc67001dd88981f3e3889a7cf94ed3777434f7ef
Ömercan <omercankara3434@gmail.com>
2024-07-18 17:59:47 +0300

    userDetails page finali
    userDetails page finali

--- components/UserDetailComponent.js
+++ components/UserDetailComponent.js
@@ -6,7 +6,6 @@ export default function UserDetailComponent({ userDetail }) {
     console.log(userDetail);
   }, [userDetail]);
 
- 
   if (!userDetail) {
     return (
       <View style={styles.container}>
@@ -15,13 +14,29 @@ export default function UserDetailComponent({ userDetail }) {
     );
   }
 
-
   return (
     <View style={styles.container}>
+
       <View style={styles.userHeader}>
+
         <View style={styles.userContent}>
           <Image source={{ uri: userDetail.avatar }} style={styles.userImage} />
-          <Text>{userDetail.email}</Text>
+          <Text style={styles.userName}>
+            {userDetail.first_name} {userDetail.last_name}
+          </Text>
+        </View>
+
+        <View style={styles.userInfo}>
+          <View style={styles.infoItem}>
+            <Text style={styles.infoLabel}>Adı Soyadı</Text>
+            <Text style={styles.infoText}>
+              {userDetail.first_name} {userDetail.last_name}
+            </Text>
+          </View>
+          <View style={styles.infoItem}>
+            <Text style={styles.infoLabel}>E-posta</Text>
+            <Text style={styles.infoText}>{userDetail.email}</Text>
+          </View>
         </View>
       </View>
     </View>
@@ -31,21 +46,18 @@ export default function UserDetailComponent({ userDetail }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    
   },
   userHeader: {
     width: "100%",
-    height: 100,
-    
-    justifyContent: "center",
-    backgroundColor: "yellow",
+    padding: 20,
+    backgroundColor: "#f0f0f0",
+   
   },
   userContent: {
     flexDirection: "row",
     alignItems: "center",
+    marginBottom: 10,
     
-    padding: 10,
-    borderRadius: 8,
   },
   userImage: {
     width: 80,
@@ -53,5 +65,29 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
     borderRadius: 40,
     marginRight: 10,
+  },
+  userName: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  userInfo: {
+    marginTop: 20,
+    
+  },
+  infoItem: {
+    marginBottom: 10,
+  },
+  infoLabel: {
+    fontWeight: "bold",
+    marginBottom: 5,
+    
+  },
+  infoText: {
+    fontSize: 16,
+    backgroundColor:"#EBE9E9",
+    width:'60%',
+    height:40,
+    borderRadius:10,
+    padding:10
   },
 });
