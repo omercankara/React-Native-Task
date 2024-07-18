b4ac7e5fd44437bf69667355aefda8d57acb58a2
Ömercan <omercankara3434@gmail.com>
2024-07-18 13:45:04 +0300

    React Native Task
    React Native Task

---
+++ src/Services/Api.js
@@ -0,0 +1,15 @@
+import axios from "axios"
+
+const BASE_URL = 'https://reqres.in/api/';
+
+
+//Kullanıcıları çek
+export const fetchUser = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/`);
+      return response.data;
+    } catch (error) {
+      console.error('Error: ', error);
+      throw error;
+    }
+  };
